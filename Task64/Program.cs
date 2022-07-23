508a402abeb0e9a9f29f7a6381a9af47bc494667
Konstantin <paradyx@outlook.com>
2022-07-23 21:16:21 +0800

    add homework Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,43 @@
+﻿void Recursion(int start, int end)
+{
+    if (start > end)
+    {
+        return;
+    }
+    Console.Write($"{start++} ");
+
+    Recursion(start, end);
+}
+int startNumber = 0;
+int endNumber = 0;
+string programmName = "|  ВЫВОД ПОРЯДКОВЫХ ЧИСЕЛ ОТ И ДО  |";
+string intError = "ОШИБКА! ВЫ ВВЕЛИ НЕ ЦЕЛОЧИСЛЕННОЕ ЗНАЧЕНИЕ\n";
+string line = new string('=', programmName.Length);
+string message1 = "Введите стартовое число: ";
+string message2 = "Введите конечное число: ";
+Console.Clear();//Очищаем консоль
+/*ЗАГОЛОВОК (ДЛЯ КРАСОТЫ)*/
+Console.WriteLine($"{line}\n{programmName}\n{line}\n");//Название
+/*КОНЕЦ ЗАГОЛОВКА*/
+Console.Write(message1);
+while (!int.TryParse(Console.ReadLine(), out startNumber))
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(intError);
+    Console.ResetColor();
+    Console.Write(message1);
+}
+Console.Write(message2);
+while (!int.TryParse(Console.ReadLine(), out endNumber))
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(intError);
+    Console.ResetColor();
+    Console.Write(message2);
+}
+Console.WriteLine("\n" + line);
+Console.WriteLine($"Вывод порядковых номеров от {startNumber} до {endNumber}");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Recursion(startNumber, endNumber);
+Console.ResetColor();
+Console.WriteLine("\n" + line + "\n");
